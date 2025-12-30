f6d29fffa9be04432db99fc74ae79f29fd8ed0e9
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-12-30 10:45:33 +0000

    Move assert function to separate file

---
+++ assert.sh
@@ -0,0 +1,10 @@
+assert () {
+	# $1 - command to be executed
+	# $2 - message to print if command fails
+	eval "$1"
+
+	if test $? -ne 0
+	then
+		printf '%s\n' "$2"
+	fi
+}
--- test_assert.sh
+++ test_assert.sh
@@ -1,15 +1,5 @@
 . ./run.sh
-
-assert () {
-	# $1 - command to be executed
-	# $2 - message to print if command fails
-	eval "$1"
-
-	if test $? -ne 0
-	then
-		printf '%s\n' "$2"
-	fi
-}
+. ./assert.sh
 
 test_prints_message_on_failure () {
 	cmd='false'
