ab0f8ba2d95174080216219740c097f7e24f8f53
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-12-30 17:05:48 +0000

    Add error message to assert exit code

--- assert_exit_code.sh
+++ assert_exit_code.sh
@@ -2,5 +2,12 @@ assert_exit_code () {
 	# $1 - expected exit code
 	# $2 - command to be executed
 	( eval "$2" )
-	test $? -eq "$1"
+
+	set -- "$@" $?  # $3 - actual exit code of command
+
+	if test "$3" -ne "$1"
+	then
+		printf "expected: '%s'\nactual: '%s'\n" "$1" "$3"
+		return 1
+	fi
 }
--- test_assert_exit_code.sh
+++ test_assert_exit_code.sh
@@ -1,12 +1,24 @@
 . ./run.sh
 . ./assert_exit_code.sh
 
+test_prints_message_on_failure () {
+	return_1 () {
+		return 1
+	}
+
+	msg="\
+expected: '0'
+actual: '1'"
+
+	test "$(assert_exit_code 0 'return_1')" = "$msg"
+}
+
 test_fails_if_incorrect_exit_code () {
 	return_1 () {
 		return 1
 	}
 
-	assert_exit_code 0 'return_1'
+	assert_exit_code 0 'return_1' >/dev/null
 
 	test $? -eq 1
 }
@@ -31,6 +43,7 @@ test_executes_command_in_isolation () {
 	test -z "$foo"
 }
 
+run test_prints_message_on_failure
 run test_fails_if_incorrect_exit_code
 run test_succeeds_if_correct_exit_code
 run test_executes_command_in_isolation
