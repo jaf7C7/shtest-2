c6b8edc59196ab2bd593eb40c41abef81ac24521
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-12-30 15:28:52 +0000

    Make assert_equal print a message if strings are not equal

--- assert_equal.sh
+++ assert_equal.sh
@@ -1,4 +1,8 @@
 assert_equal () {
 	# $1, $2 - strings to compare
-	test "$1" = "$2"
+	if test "$1" != "$2"
+	then
+		printf "expected: '%s'\nactual: '%s'\n\n" "$1" "$2"
+		return 1
+	fi
 }
--- test_assert_equal.sh
+++ test_assert_equal.sh
@@ -2,7 +2,7 @@
 . ./assert_equal.sh
 
 test_fails_if_strings_are_not_equal () {
-	assert_equal 'string' 'different string'
+	assert_equal 'string' 'different string' >/dev/null
 
 	test $? -eq 1
 }
@@ -13,5 +13,19 @@ test_succeeds_if_strings_are_equal () {
 	test $? -eq 0
 }
 
+test_prints_message_if_strings_are_not_equal () {
+	string='a'
+	other_string='b'
+	msg="\
+expected: '$string'
+actual: '$other_string'\
+"
+
+	result=$(assert_equal "$string" "$other_string")
+
+	test "$result" = "$msg"
+}
+
 run test_fails_if_strings_are_not_equal
 run test_succeeds_if_strings_are_equal
+run test_prints_message_if_strings_are_not_equal
