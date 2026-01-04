e92ee6ba614a9ae589994acde706d84013e56c74
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-01-04 08:28:10 +0000

    Make sure only tests are extracted from test files

--- test_get_test_names.sh
+++ test_get_test_names.sh
@@ -16,4 +16,23 @@ EOF
 	return "$outcome"
 }
 
+test_ignores_functions_which_do_not_start_with_test_ () {
+	tmp=$(mktemp)
+
+	cat >"$tmp" <<EOF
+test_a () {
+}
+
+some_func () {
+}
+EOF
+	test "$(get_test_names "$tmp")" = 'test_a'
+	outcome=$?
+
+	rm "$tmp"
+
+	return "$outcome"
+}
+
 run test_extracts_all_test_names_from_a_file
+run test_ignores_functions_which_do_not_start_with_test_
