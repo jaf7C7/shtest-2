eb1b5d51a04387d469a972a24341eb89b7f46387
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-01-04 20:54:41 +0000

    Refactoring: Increase comprehensibility of tests

--- tests/test_main.sh
+++ tests/test_main.sh
@@ -1,101 +1,104 @@
 . shtest/main.sh
 
 test_extracts_test_names_from_a_file_and_runs_them () {
-	tmp=$(mktemp)
+	test_file=$(mktemp)
 
-	cat >"$tmp" <<EOF
+	cat >"$test_file" <<EOF
 test_a () {
 	return 0
 }
 EOF
 
-	test "$(main "$tmp")" = "\
-$tmp
+	test "$(main "$test_file")" = "\
+$test_file
 
 test_a...ok"
-	outcome=$?
 
-	rm "$tmp"
+	result=$?
+	rm "$test_file"
 
-	return "$outcome"
+	return "$result"
 }
 
 test_runs_all_tests_in_each_test_file_specified () {
-	tmp1=$(mktemp)
-	tmp2=$(mktemp)
+	test_file1=$(mktemp)
+	test_file2=$(mktemp)
 
-	cat >"$tmp1" <<EOF
+	cat >"$test_file1" <<EOF
 test_a () {
 	return 0
 }
 EOF
-	cat >"$tmp2" <<EOF
+
+	cat >"$test_file2" <<EOF
 test_b () {
 	return 0
 }
 EOF
-	test "$(main "$tmp1" "$tmp2")" = "\
-$tmp1
+
+	test "$(main "$test_file1" "$test_file2")" = "\
+$test_file1
 
 test_a...ok
 
-$tmp2
+$test_file2
 
 test_b...ok"
-	outcome=$?
 
-	rm "$tmp1" "$tmp2"
+	result=$?
+	rm "$test_file1" "$test_file2"
 
-	return "$outcome"
+	return "$result"
 }
 
 test_runs_each_test_file_in_isolation () {
-	tmp1=$(mktemp)
-	tmp2=$(mktemp)
+	test_file1=$(mktemp)
+	test_file2=$(mktemp)
 
-	cat >"$tmp1" <<EOF
+	cat >"$test_file1" <<EOF
 some_var=1
 EOF
-	cat >"$tmp2" <<EOF
+	cat >"$test_file2" <<EOF
 some_var=2
 EOF
-	main "$tmp1" "$tmp2" >/dev/null
+	main "$test_file1" "$test_file2" >/dev/null
 
 	test -z "$some_var"
-	outcome=$?
 
-	rm "$tmp1" "$tmp2"
+	result=$?
+	rm "$test_file1" "$test_file2"
 
-	return "$outcome"
+	return "$result"
 }
 
 test_ignores_functions_which_do_not_start_with_test_ () {
-	tmp=$(mktemp)
+	test_file=$(mktemp)
 
-	cat >"$tmp" <<EOF
+	cat >"$test_file" <<EOF
 test_a () {
-	:
+	return 0
 }
 
 some_func () {
-	:
+	return 0
 }
 EOF
-	test "$(main "$tmp")" = "\
-$tmp
+
+	test "$(main "$test_file")" = "\
+$test_file
 
 test_a...ok"
-	outcome=$?
 
-	rm "$tmp"
+	result=$?
+	rm "$test_file"
 
-	return "$outcome"
+	return "$result"
 }
 
 test_runs_each_test_function_in_isolation () {
-	tmp=$(mktemp)
+	test_file=$(mktemp)
 
-	cat >"$tmp" <<\EOF
+	cat >"$test_file" <<\EOF
 test_a () {
 	a=1
 }
@@ -104,76 +107,69 @@ test_b () {
 	test -z "$a"
 }
 EOF
-	test "$(main "$tmp")" = "\
-$tmp
+	test "$(main "$test_file")" = "\
+$test_file
 
 test_a...ok
 test_b...ok"
-	outcome=$?
+	result=$?
 
-	rm "$tmp"
+	rm "$test_file"
 
-	return "$outcome"
+	return "$result"
 }
 
 test_also_extracts_function_names_with_different_valid_formatting () {
-	tmp=$(mktemp)
+	test_file=$(mktemp)
 
-	cat >"$tmp" <<EOF
+	cat >"$test_file" <<EOF
 test_a() {
-	:
+	return 0
 }
 
 test_b ()
 {
-	:
+	return 0
 }
 
 test_c()
 {
-	:
+	return 0
 }
 EOF
-	expected="\
-$tmp
+	test "$(main "$test_file")" = "\
+$test_file
 
 test_a...ok
 test_b...ok
 test_c...ok"
-	result="$(main "$tmp")"
 
-	test "$result" = "$expected"
-	outcome=$?
+	result=$?
+	rm "$test_file"
 
-	if test "$outcome" -ne 0
-	then
-		printf '\n got: %s\nexpected: %s\n' "$result" "$expected" >&2
-	fi
-
-	rm "$tmp"
-
-	return "$outcome"
+	return "$result"
 }
 
 test_only_extracts_names_of_defined_functions () {
-	tmp=$(mktemp)
+	test_file=$(mktemp)
 
-	cat >"$tmp" <<EOF
+	cat >"$test_file" <<EOF
 test_this_is_a_real_test_function () {
 	fake_test_function='
 test_looks_like_a_test_function_but_is_not () {
-	:
+	return 0
 }
 '
 }
 EOF
-	test "$(main "$tmp")" = "\
-$tmp
+
+	test "$(main "$test_file")" = "\
+$test_file
 
 test_this_is_a_real_test_function...ok"
-	outcome=$?
 
-	rm "$tmp"
+	result=$?
+	rm "$test_file"
 
-	return "$outcome"
+	return "$result"
 }
