843ffe52abf8bac56047f8ab4a738a99f9442076
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-01-04 15:30:25 +0000

    Move function into main.sh

--- shtest/main.sh
+++ shtest/main.sh
@@ -1,5 +1,25 @@
 . shtest/run.sh
-. shtest/get_test_names.sh
+
+_get_test_names () {
+	# $1 - File containing test functions.
+	. "$1"
+	
+	# Set argument list to array of potential test names.
+	set -- $(sed -n 's/\(^test_[^ 	]*\)[ 	]*().*/\1/p' "$1")
+
+	# Filter out any names which aren't declared in the file, leaving only
+	# real tests.
+	for test in "$@"
+	do
+		shift
+		if command -v "$test" >/dev/null
+		then
+			set -- "$@" "$test"
+		fi
+	done
+
+	printf '%s\n' "$@"
+}
 
 main () {
 	for f in "$@"
@@ -9,7 +29,7 @@ main () {
 
 			printf '%s\n\n' "$f"
 
-			for t in $(get_test_names "$f")
+			for t in $(_get_test_names "$f")
 			do
 				run "$t"
 			done
